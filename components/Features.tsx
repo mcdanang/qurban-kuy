edcfadd2e936d53d63daa61497cb75bae5db7fcd
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-05-23 00:11:09 +0700

    create landing page

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,44 +1,48 @@
-import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+import Image from "next/image";
 
 const features = [
   {
-    name: 'Push to deploy',
+    name: "Domba",
     description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-    icon: CloudArrowUpIcon,
+      "Kami menyediakan domba jenis Priangan, Garut, Lokal, atau menyesuaikan ketersediaan.",
+    svg: "/svgs/sheep.svg",
   },
   {
-    name: 'SSL certificates',
+    name: "Sapi",
     description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LockClosedIcon,
+      "Kami menyediakan sapi jenis Limosin, Simental, Pegon, Peranakan Ongole, Bali, atau menyesuaikan ketersediaan.",
+    svg: "/svgs/cow.svg",
   },
   {
-    name: 'Simple queues',
+    name: "Gratis Ongkir",
     description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: ArrowPathIcon,
+      "Harga hewan sudah termasuk gratis ongkir untuk wilayah kota Jabodetabek. Di luar itu, akan ada biaya tambahan.",
+    svg: "/svgs/truck.svg",
   },
   {
-    name: 'Advanced security',
+    name: "Titip Salur",
     description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: FingerPrintIcon,
+      "Kami dapat membantu anda untuk mencari lokasi untuk penyembelihan hewan qurban anda, tetapi menyembelih sendiri hewan qurban itu paling afdhol",
+    svg: "/svgs/phone.svg",
   },
-]
+];
 
 export default function Features() {
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Deploy faster</h2>
+          <h2 className="text-lg font-semibold leading-8 tracking-tight text-choco">
+            Qurban lebih mudah
+          </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Everything you need to deploy your app
+            Kami siap menyediakan hewan qurban terbaik dengan gratis ongkir
+            untuk wilayah Jabodetabek
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+            Tidak perlu lagi repot mengurus pembelian hewan qurban, cukup pesan
+            via Whatsapp/Instagram dan akan kami antarkan hewan qurban sesuai
+            pesanan ke depan rumah anda pada H-1 Idul Adha 2023
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
@@ -46,17 +50,25 @@ export default function Features() {
             {features.map((feature) => (
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-base font-semibold leading-7 text-gray-900">
-                  <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                  <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-choco">
+                    <Image
+                      src={feature.svg}
+                      alt="App screenshot"
+                      width={50}
+                      height={50}
+                      className="w-[1.6rem]"
+                    />
                   </div>
                   {feature.name}
                 </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                <dd className="mt-2 text-base leading-7 text-gray-600">
+                  {feature.description}
+                </dd>
               </div>
             ))}
           </dl>
         </div>
       </div>
     </div>
-  )
+  );
 }
---
+++ components/PricingSapi.tsx
@@ -0,0 +1,82 @@
+const tiers = [
+  {
+    name: "Sapi Small",
+    href: "#",
+    size: "text-4xl",
+    weight: "±300KG",
+    price: "23.1 juta/ekor",
+    best: false,
+  },
+  {
+    name: "Sapi Medium",
+    href: "#",
+    size: "text-5xl",
+    weight: "±390KG",
+    price: "28 juta/ekor",
+    best: true,
+  },
+  {
+    name: "Sapi Large",
+    href: "#",
+    size: "text-6xl",
+    weight: "±480KG",
+    price: "33.6 juta/ekor",
+    best: false,
+  },
+];
+
+export default function PricingSapi() {
+  return (
+    <div className="bg-choco" id="harga-sapi">
+      <div className="px-6 pt-12 lg:px-8 lg:pt-20">
+        <div className="text-center">
+          {/* <h2 className="text-xl font-semibold leading-6 text-gray-300">
+            Pricing
+          </h2> */}
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl lg:text-5xl">
+            Price List Sapi
+          </p>
+          <p className="mx-auto mt-3 max-w-4xl text-xl text-gray-300 sm:mt-5 sm:text-2xl">
+            Pilih Sapi Qurban sesuai yang kamu inginkan
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-16 bg-white pb-12 lg:mt-20 lg:pb-20">
+        <div className="relative z-0">
+          <div className="absolute inset-0 h-5/6 bg-choco lg:h-2/3" />
+          <div className="mx-auto max-w-7xl px-10 lg:px-8">
+            <div className="relative mt-12 space-y-4 sm:mt-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:mx-auto lg:max-w-4xl">
+              {tiers.map((tier) => (
+                <>
+                  <div
+                    key={tier.name}
+                    className="divide-y divide-gray-200 max-w-md rounded-3xl border-black border-4 shadow-sm bg-white"
+                  >
+                    <div className="p-2 h-80 lg:h-[420px] flex items-center justify-center">
+                      <h2
+                        className={`font-fredoka font-semibold uppercase text-center [word-spacing:999px] ${tier.size}`}
+                      >
+                        {tier.name}
+                      </h2>
+                    </div>
+                    <div className="relative border-none">
+                      <div className="absolute bottom-5 right-10 font-blogger text-xl lg:text-3xl">
+                        {tier.weight}
+                      </div>
+                    </div>
+                    <div className="relative border-none">
+                      <div className="absolute bottom-5 left-5 text-center font-monts font-semibold text-xl lg:text-2xl">
+                        {tier.price}
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
