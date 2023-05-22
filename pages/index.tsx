a5ee852b6e8a74a665bc3243edda8614aafd1f8f
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-05-22 14:22:16 +0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import CallToAction from "@/components/CallToAction";
+import Header from "@/components/Header";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Features/>
+      <Pricing/>
+      <CallToAction/>
+      <Footer/>
+    </>
+  )
+}
